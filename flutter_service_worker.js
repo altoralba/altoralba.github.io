4c87a7058844e547c507dd88609f5d00478d1f40
altoralba <altoralba86@gmail.com>
2022-01-31 19:20:36 +0800

    Fixed orientation on mobile phones

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "22e13319077cd57333ae8b816aff8b39",
-"/": "22e13319077cd57333ae8b816aff8b39",
-"main.dart.js": "180f0edb9af4e3d1f68e99770f4e51bb",
+"index.html": "7732c270676875873fd9f8f60085e2c2",
+"/": "7732c270676875873fd9f8f60085e2c2",
+"main.dart.js": "c07e580deb4ca246a2acacb88f1b660d",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
